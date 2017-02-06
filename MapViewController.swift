ced1b355ab6fec26c26dd28168408b9646114a9e
James Marlowe <jam03marlowe@gmail.com>
2017-02-06 12:28:21 -0500

    2/6/17 - Chapter 7 for World Trotter

---
+++ MapViewController.swift
@@ -0,0 +1,9 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by James Marlowe on 2/5/17.
+//  Copyright © 2017 High Point University. All rights reserved.
+//
+
+import Foundation
